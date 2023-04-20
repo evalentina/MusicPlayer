d22748907833643df2819ff241c6a358e735d199
Evdokimova Valentina <evdokimovalentina00@gmail.com>
2023-04-20 19:15:24 +0300

    Initial Commit

---
+++ MusicPlayer/MusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerApp.swift
+//  MusicPlayer
+//
+//  Created by Валентина Евдокимова on 20.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
